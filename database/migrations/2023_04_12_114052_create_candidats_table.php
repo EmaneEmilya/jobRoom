043416e07b7800c92f1b688d84c7f8984ff8cae1
lamiae8coder <130150586+lamiae8coder@users.noreply.github.com>
2023-04-12 12:24:22 +0000

    candidat change

--- database/migrations/2023_04_12_114052_create_candidats_table.php
+++ database/migrations/2023_04_12_114052_create_candidats_table.php
@@ -14,8 +14,16 @@ class CreateCandidatsTable extends Migration
     public function up()
     {
         Schema::create('candidats', function (Blueprint $table) {
-            $table->id();
+            $table->id_candidat();
+            $table->string('fullName');
+            $table->string('email');
+            $table->string('password');
+            $table->string('contact');
+            $table->string('city');
+            $table->text('region');
             $table->timestamps();
+            $table->unsignedBigInteger('recruiter_id')->nullable();
+            $table->foreign('recruiter_id')->references('id_recruiter')->on('recruiters');
         });
     }
 
